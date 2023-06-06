4c7dc3810cc466542f7c5e23a0b93e2b8866df3c
RSantosIgor <s.igorrosa@gmail.com>
2023-06-06 02:24:30 -0300

    setup project

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './style';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
---
+++ src/pages/Search/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './style';
+
+export default function Search() {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  )
+}
